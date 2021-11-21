0eeedc53c9c4c23876c0a6193095089b8d0f08f4
Justyna Pruś <65462428+JustynaPrus@users.noreply.github.com>
2021-11-21 20:46:16 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,18 +6,22 @@ const plus = document.querySelector(".plus");
 const counter = document.querySelector(".counter");
 const basket = document.querySelector(".basket");
 const basketCard = document.querySelector(".basket-card");
-const cardBody = document.querySelector(".card-body");
+const basketBody = document.querySelector(".basket-card-body");
 const btnAddToCard = document.querySelector(".btn-add-to-card");
 const basketContainer = document.querySelector(".basket-container");
-const carousel = document.querySelector(".carousel");
+const carousel = document.querySelector(".wrapper");
 const basketText = document.querySelector(".basket-text");
+const item = document.querySelector(".item");
+const iconClose = document.querySelector(".close");
+const imgIndicators = [...document.querySelectorAll(".img-indicators")];
+const navbarToggler = document.querySelector(".navbar-toggler");
+const navbarNav = document.querySelector(".navbar-nav");
 
 let number = 0;
 
 const Buy = () => {
   counter.textContent = `${number}`;
   basket.textContent = `${number}`;
-  basket.style.display = "block";
 };
 
 basketContainer.addEventListener("click", function () {
@@ -55,27 +59,48 @@ thumbnail.forEach((e) => {
   });
 });
 
+imgIndicators.forEach((e) => {
+  e.addEventListener("mouseover", function () {
+    e.classList.add("opacity");
+  });
+  e.addEventListener("mouseout", function () {
+    e.classList.remove("opacity");
+  });
+});
+
 mainImg.addEventListener("click", function () {
   carousel.classList.toggle("gallery");
 });
 
-/*carousel.addEventListener("mouseout", function () {
-  carousel.classList.remove("gallery");
-});*/
+iconClose.addEventListener("click", function () {
+  carousel.classList.toggle("gallery");
+});
 
 btnAddToCard.addEventListener("click", function () {
-  const item = document.createElement("div");
-  cardBody.innerHTML = "";
-  item.innerHTML = `<div>
-  <div class="d-flex align-items-center">
-    <img class="basket-img" src="/images/image-product-1-thumbnail.jpg">
-    <div class="m-3">
-      <p class="m-0">Fall Limited Edition Sneakers</p>
-      <p class="m-0">$125.00 x ${number} $${125 * number}</p>
-    </div>
-    <img src="/images/icon-delete.svg">
-  </div>
-  <button class="btn btn-add-to-card py-2">Checkout</button>
-</div>`;
-  cardBody.appendChild(item);
+  basket.style.display = "block";
+  basketText.classList.add("hide");
+  item.classList.remove("hide");
+  basketBody.innerHTML = `
+  <div class="item">
+          <div class="d-flex align-items-center">
+            <img class="basket-img" src="/images/image-product-1-thumbnail.jpg">
+            <div class="m-3">
+            <p class="m-0">Fall Limited Edition Sneakers</p>
+            <p class="m-0">$125.00 x ${number} $${125 * number}</p></div>
+            <img class="icon-delete" src="/images/icon-delete.svg">
+          </div>
+          <button class="btn btn-checkout py-2">Checkout</button>
+        </div>`;
+
+  const iconDelete = document.querySelector(".icon-delete");
+
+  iconDelete.addEventListener("click", function () {
+    basket.textContent = "0";
+    basketText.classList.remove("hide");
+    basketBody.innerHTML = `<p class="card-text basket-text">Your cart is empty.</p>`;
+  });
+});
+
+navbarToggler.addEventListener("click", function () {
+  navbarNav.classList.toggle("gallery");
 });
